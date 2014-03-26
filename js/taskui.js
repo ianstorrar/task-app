ec985d2bde721f08adb5ef68227f145625aeb98f
Ian Storrar <ianstorrar@gmail.com>
2014-03-26 01:14:45 -0400

    Added task.priority to task object

--- js/taskui.js
+++ js/taskui.js
@@ -18,6 +18,7 @@ $( document ).on( "mobileinit", function () {
             '<a href="#">',
             '<h2>', task.name, '</h2>',
             '<p>', task.details, '</p>',
+			'<p>', task.priority, '</p>',
             '</a>',
             '<a href="#">',
             'Show',
